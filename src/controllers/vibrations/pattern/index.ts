ced4fe2da8a7117295655ed1f4ded7834362d718
Julian Pasquale <julianagustin78@gmail.com>
2021-03-27 12:02:05 -0300

    Dynamic period based on vibration's duration

--- src/controllers/vibrations/pattern/index.ts
+++ src/controllers/vibrations/pattern/index.ts
@@ -7,22 +7,18 @@ import patternSamples from './samples';
 import DutyCycle, { DUTY_CYCLES } from './duty_cycle';
 
 /**
- * temporary fixed
- */
-// const DURATION = 1000;
-
-/**
  * Get vibrations details in pattern format from Firestore.
  */
-
 export default (_req: Request, res: Response, _next: NextFunction): void => {
   const samples = patternSamples(res.locals.vibration.data.pattern);
 
-  let duty_cycle = DutyCycle(samples[0]);
+  const period = res.locals.vibration.data.duration * 1000 / samples.length
+
+  const duty_cycle = DutyCycle(samples[0]);
   let result: number[] = [];
 
   for (let index = 1; index < samples.length; index++) {
-    result = result.concat(dutyCycleToPWM(duty_cycle.current));
+    result = result.concat(dutyCycleToPWM(duty_cycle.current, period));
 
     if ((samples[index] == samples[index - 1])) continue;
 
@@ -36,23 +32,21 @@ export default (_req: Request, res: Response, _next: NextFunction): void => {
   res.send(result);
 };
 
-const dutyCycleToPWM = (value: number) => {
+const dutyCycleToPWM = (value: number, period: number) => {
   if (value == DUTY_CYCLES.STOPPED)
-    return [0, 100];
+    return [0, period];
   if (value == DUTY_CYCLES.LOWER)
-    return [25, 75];
+    return [period * 0.25, period * 0.75];
   if (value == DUTY_CYCLES.MIDDLE_1)
-    return [50, 50];
+    return [period * 0.5, period * 0.5];
   if (value == DUTY_CYCLES.MIDDLE_2)
-    return [75, 25];
+    return [period * 0.75, period * 0.25];
 
   // (value == DUTY_CYCLES.HIGHER)
-  return [100, 0];
+  return [period, 0];
 };
 
 /**
  * Create 4 posible values. Values are in range 0..200.
  */
 // const quantify = (value: number) => Math.floor(value / 50);
-
-// const toBinary = (value: number) => value.toString(2);
